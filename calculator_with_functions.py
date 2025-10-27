68bc1fe183377bbb6ee8d05a0a2502cfa7a780fe
saiteja samidala <teja.samidala@gmail.com>
2025-10-27 00:05:17 -0500

    Add calculator_with_functions program and output screenshot

---
+++ calculator_with_functions.py
@@ -0,0 +1,17 @@
+def calculate(a, b, operation):
+    if operation == "add":
+        return a + b
+    elif operation == "subtract":
+        return a - b
+    elif operation == "multiply":
+        return a * b
+    elif operation == "divide":
+        if b != 0:
+            return a / b
+        else:
+            return "Cannot divide by zero!"
+    else:
+        return "Invalid operation!"
+
+result = calculate(1, 9, "multiply")
+print("Result:", result)
